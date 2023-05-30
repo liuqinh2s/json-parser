fcb0c18ed00eadc76ae5784c89c37e4ff399e50d
liuqin <liuqinh2s@gmail.com>
2023-05-30 14:47:27 +0800

    去掉vue，简化此项目，直接用tsc和rollup得到最终的js

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 import JsonParser from "./jsonParser";
-import json from "./china.json";
-
+import json from "./china";
 let jsonParser = new JsonParser();
-
 // 测试 unicode 转码
 // let str = jsonParser.stringify(json);
 // let str = '["\\u89E3\\u6790\\u5668"]';
 // let obj = jsonParser.parse(str);
-
 // 测试undefined
 let a;
 let obj = { k: a, kk: "h" };
@@ -15,3 +12,5 @@ console.log(JSON.stringify(a));
 console.log(jsonParser.stringify(a));
 console.log(JSON.stringify(obj));
 console.log(jsonParser.stringify(obj));
+console.log(jsonParser.stringify(json));
+console.log(jsonParser.parse(jsonParser.stringify(json) || ""));
---
+++ main.ts
@@ -0,0 +1,19 @@
+import JsonParser from "./jsonParser";
+import json from "./china";
+
+let jsonParser = new JsonParser();
+
+// 测试 unicode 转码
+// let str = jsonParser.stringify(json);
+// let str = '["\\u89E3\\u6790\\u5668"]';
+// let obj = jsonParser.parse(str);
+
+// 测试undefined
+let a;
+let obj = { k: a, kk: "h" };
+console.log(JSON.stringify(a));
+console.log(jsonParser.stringify(a));
+console.log(JSON.stringify(obj));
+console.log(jsonParser.stringify(obj));
+console.log(jsonParser.stringify(json));
+console.log(jsonParser.parse(jsonParser.stringify(json) || ""));
